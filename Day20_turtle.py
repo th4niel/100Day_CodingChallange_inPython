a67ea95ed50a5d99e8e4919c410a5dd776f8a481
ThaN1eL <yuiharabusiness@gmail.com>
2025-07-28 21:43:13 +0800

    OOP

---
+++ Day20_turtle.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+
+edwin = Turtle()
+print(edwin)
+edwin.shape("turtle")
+edwin.color("coral")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
--- test.py
+++ test.py
@@ -1,16 +1,26 @@
-def logic_skill():
-    brain_score = 0
-    practice_score = 0
+# def logic_skill():
+#     brain_score = 0
+#     practice_score = 0
     
-    while True:
-        brain_score += 1
-        practice_score += 1
+#     while True:
+#         brain_score += 1
+#         practice_score += 1
         
-        print(f"\nProgress: Brain = {brain_score}, Practice = {practice_score}")
-        if brain_score == 100 and practice_score == 100:
-            print("🎉 Congratulations! Finally your hard work is paying off!")
-            print("Practice makes perfect!!")
-            break
+#         print(f"\nProgress: Brain = {brain_score}, Practice = {practice_score}")
+#         if brain_score == 100 and practice_score == 100:
+#             print("🎉 Congratulations! Finally your hard work is paying off!")
+#             print("Practice makes perfect!!")
+#             break
             
-        print("Keep going! The journey towards 100 continues....\n")
-logic_skill()
+#         print("Keep going! The journey towards 100 continues....\n")
+# logic_skill()
+
+
+# class mySkill():
+#     def __init__(self, name):
+#         self.name = name
+    
+
+# skill = mySkill("Nathan")
+
+# print(skill.name)
