9ffaf29651c7c91787aaabc91dd61eb085512711
ThaN1eL <yuiharabusiness@gmail.com>
2025-08-04 22:08:33 +0800

    try

---
+++ turtleMain.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+
+win = Turtle()
+win.shape("turtle")
+win.color("green")
+
+for x in range(4):
+    win.forward(100)
+    win.right(90)
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
