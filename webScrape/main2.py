b1b94e5e6c17cdfb3558dddc4a8904f059f616a1
ThaN1eL <yuiharabusiness@gmail.com>
2025-10-06 18:12:23 +0800

    webscraping

--- webScrape/main2.py
+++ webScrape/main2.py
@@ -19,7 +19,13 @@ for title  in title_text:
     
 article_upvote = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
 
+largest_number = max(article_upvote)
+index_number = article_upvote.index(largest_number)
+print(article_texts[index_number])
+print(article_links[index_number])
+print(largest_number)
 
-print(article_texts)
-print(article_links)
-print(article_upvote)
+
+# print(article_texts)
+# print(article_links)
+# print(article_upvote)
