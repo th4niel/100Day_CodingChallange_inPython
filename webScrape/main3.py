7454cca6bb46e5eba94ab5d260e4e50cc88d5874
ThaN1eL <yuiharabusiness@gmail.com>
2025-10-06 19:23:37 +0800

    web scrapping

---
+++ webScrape/main3.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+all_movies = soup.find_all(name="h3", class_="title")
+
+movies_title = [movie.getText() for movie in all_movies]
+
+print(movies_title)
